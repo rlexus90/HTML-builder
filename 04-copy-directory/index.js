1087a68a874a9c2ba625483d1a082d9b582ffd5c
rlexus90 <rlexus90@gmail.com>
2023-05-07 19:41:54 +0300

    init: done 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,54 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+const dir = path.join(__dirname, 'files');
+const copyDir = path.join(__dirname, 'files-copy');
+
+async function copy() {
+	await fs.mkdir(copyDir, { recursive: true });
+	const f = async () => {
+		const copyFiles = await fs.readdir(dir);
+		copyFiles.forEach((file) => {
+			fs.copyFile(path.join(dir, file), path.join(copyDir, file));
+		})
+	};
+	await f();
+
+	const actual = async () => {
+		const currentFiles = await fs.readdir(dir);
+		const copyFiles = await fs.readdir(copyDir);
+		const notFound = copyFiles.filter(file => {return !currentFiles.includes(file) });
+		if (notFound){
+			notFound.forEach(file=>{
+				fs.rm(path.join(copyDir,file));
+			})
+		}
+	}
+	await actual();
+	console.log('All done!')
+};
+
+
+copy();
+
+
+// fs.readdir(copyDir, (err, files) => {
+// 	if (err) throw err;
+// 	if (files) files.forEach(file => {
+// 		fs.rm(path.join(copyDir, file), err => {
+// 			if (err) throw err;
+// 		});
+// 	});
+// });
+
+// fs.readdir(dir, (err, files) => {
+// 	if (err) throw err;
+// 	files.forEach(file => {
+// 		const currentFile = path.join(dir, file);
+// 		const copyFile = path.join(copyDir, file);
+// 		fs.copyFile(currentFile, copyFile, err => {
+// 			if (err) throw err;
+// 		});
+// 	});
+// 	console.log('Files copy done');
+// });
